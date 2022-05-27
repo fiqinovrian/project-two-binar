39d4ddf582db7070c1fdf21d77d027015be72999
fiqinovrian <fiqinovrian@gmail.com>
2022-05-27 19:44:22 +0700

    Project Two isinya export dan import fungsi luas segitiga

---
+++ index.js
@@ -0,0 +1,2 @@
+const luasSegitiga = require('./segitiga.js');
+console.log(luasSegitiga(4,10));
---
+++ segitiga.js
@@ -0,0 +1,5 @@
+//ini file segitiga.js
+function luasSegitiga(alas, tinggi) {
+    return alas * tinggi / 2;
+}
+module.exports = luasSegitiga;
